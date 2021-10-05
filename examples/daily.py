08cd3201cf2fc5087acdd7048ae213553a062501
Thomas Cellerier <thomas.cellerier@appgate.com>
2021-10-05 12:12:23 +0200

    Add daily example using timezone

---
+++ examples/daily.py
@@ -0,0 +1,32 @@
+import asyncio
+import sys
+
+if sys.version_info >= (3, 9):
+    from zoneinfo import ZoneInfo
+else:
+    from pytz import timezone as ZoneInfo
+
+from acron.scheduler import Scheduler, Job
+
+
+async def do_the_thing():
+    print("Doing the thing")
+
+
+async def run_jobs_forever():
+    do_thing = Job(
+        name="Do the thing daily",
+        schedule="11 12 * * *",
+        func=do_the_thing,
+    )
+
+    async with Scheduler(ZoneInfo("Europe/Stockholm")) as scheduler:
+        await scheduler.update_jobs({do_thing})
+        await scheduler.wait()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(run_jobs_forever())
+    except KeyboardInterrupt:
+        print("Bye.")
